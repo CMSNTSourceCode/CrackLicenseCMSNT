3c7723b33597a22bd3b31b72a66815703a6ff25b
Mai Huy Bảo <justonly.maihuybao@gmail.com>
2022-08-17 22:07:36 +0700

    Update SHOPCLONE6.php

--- SHOPCLONE6.php
+++ SHOPCLONE6.php
@@ -3,7 +3,6 @@ define("IN_SITE", true);
 require_once(__DIR__.'/libs/db.php');
 require_once(__DIR__.'/config.php');
 require_once(__DIR__.'/libs/helper.php');
-require_once(__DIR__.'/models/is_admin.php');
 $CMSNT = new DB();
 
 
